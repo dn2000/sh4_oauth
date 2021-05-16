c0b7889109000e7b82e13d8377eac7a8aec6885c
Бодрый Бородач <unitdesign2014@gmail.com>
2021-05-16 16:21:35 +0300

    add omniauth strategy

--- lib/omniauth/sh4.rb
+++ lib/omniauth/sh4.rb
@@ -1,10 +1,43 @@
 # frozen_string_literal: true
 
 require_relative "sh4/version"
+require 'omniauth-oauth2'
 
-module Omniauth
-  module Sh4
-    class Error < StandardError; end
-    # Your code goes here...
+module OmniAuth
+  module Strategies
+    class Sh4 < OmniAuth::Strategies::OAuth2
+      option :name, "sh4"
+
+      option :client_options, {
+        :site => 'http://localhost:4444',
+        :authorize_url => '/oauth/authorize',
+        :token_url => '/oauth/token'
+      }
+
+      uid { raw_info["id"] }
+
+      info do
+        {
+          email: raw_info["email"]
+        }
+      end
+
+      extra do
+        {
+          "raw_info": raw_info
+        }
+      end
+
+      def raw_info
+        @raw_info ||= access_token.get("/api/v1/me").parsed
+      end
+
+      def callback_url
+        full_host + script_name + callback_path
+      end
+
+    end
   end
 end
+
+OmniAuth.config.add_camelization "sh4", "Sh4"
